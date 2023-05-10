0154f0d33067d04c7767615d7cb2220df98286ca
Ashraf Abu Bakr <ashraf.abubakr@itworxedu.com>
2023-05-11 02:07:27 +0300

    - Added .app in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-// var ParseDashboard = require('parse-dashboard');
+var ParseDashboard = require('parse-dashboard');
 var parseServerConfig = require('parse-server-azure-config');
 var url = require('url');
 
@@ -10,8 +10,8 @@ var config = parseServerConfig(__dirname);
 
 var app = express();
 app.use('/public', express.static(__dirname + '/public'));
-app.use('/parse', new ParseServer(config.server));
-// app.use('/parse-dashboard', ParseDashboard(config.dashboard, true));
+app.use('/parse', new ParseServer(config.server.app));
+app.use('/parse-dashboard', ParseDashboard(config.dashboard, true));
 
 app.listen(process.env.PORT || url.parse(config.server.serverURL).port, function () {
   console.log(`Parse Server running at ${config.server.serverURL}`);
