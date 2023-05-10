ce82099eec9a40decccd7918ddc9af97fe728110
Ashraf Abu Bakr <ashraf.abubakr@itworxedu.com>
2023-05-11 01:06:51 +0300

    - Added app keys.

--- local.js
+++ local.js
@@ -1,5 +1,12 @@
 module.exports = {
-  server: {},
+  server: {
+    APP_ID: "06c048d2-221c-4528-b792-bcd0ef20dadc",
+    DATABASE_URI: "mongodb+srv://tkdev:mangoDB2020@tktest.oz42r.mongodb.net/?retryWrites=true&w=majority",
+    MASTER_KEY: "34d13dd8-ff4f-42f6-8c35-df20a8e27531",
+    PARSE_DASHBOARD_ALLOW_INSECURE_HTTP: "1",
+    PUBLIC_SERVER_URL: "https://xteacherkitpoc.azurewebsites.net/parse",
+    SERVER_URL: "https://xteacherkitpoc.azurewebsites.net"
+  },
   dashboard: {},
   storage: {},
   push: {}
